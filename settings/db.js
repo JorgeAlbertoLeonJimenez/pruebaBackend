1cff99ffa80318d8cdb41cf4661959ce6490fcff
jorge alberto <jorgealbertolejim@gmail.com>
2024-09-26 23:24:00 -0700

    add host

--- settings/db.js
+++ settings/db.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import { createPool } from "mysql2/promise";
 config()
 const db = createPool({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
