88993429bb05ed49ccc88ed1b528ad89cee17827
jorge alberto <jorgealbertolejim@gmail.com>
2024-09-26 23:38:27 -0700

    add db_port

--- settings/db.js
+++ settings/db.js
@@ -6,6 +6,7 @@ const db = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 try {
   console.log("se esta usando la bd ✅ ");
